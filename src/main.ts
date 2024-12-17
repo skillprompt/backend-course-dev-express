893c14fa1bbdfdf5aa71dba0cd25c015660afc7c
baijanaththaru-skillprompt <baijanaththaru@skillprompt.com>
2024-12-17 16:30:53 +0545

    init express app

---
+++ src/main.ts
@@ -0,0 +1,55 @@
+import express from "express";
+
+const todos = [];
+
+const app = express();
+
+// parser json
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.write("hello world from express app!");
+  res.end();
+});
+// -> request
+app.post(
+  "/todos/create",
+  (req, res, next) => {
+    console.log("the request comes here 1");
+
+    const currentTimeInMs = Date.now(); // unix - 1970-01-01
+
+    if (currentTimeInMs < new Date("2024-12-18").getTime()) {
+      next();
+    } else {
+      res.json({
+        message: "you cannot send request today.",
+      });
+    }
+  },
+  (req, res, next) => {
+    console.log("the request comes here 2");
+    next();
+  },
+  (req, res) => {
+    console.log("request comes here create the todos 3");
+
+    const data = req.body;
+
+    console.log("body received", data); // { name: 'expressjs tutorial', status: 'in progress' }
+
+    todos.push({
+      id: todos.length + 1,
+      name: data.name,
+      status: data.status,
+    });
+
+    res.json({
+      message: "Todos created!",
+    }); // -> response
+  }
+);
+
+app.listen(4000, () => {
+  console.log("Started server on http://localhost:4000");
+});
